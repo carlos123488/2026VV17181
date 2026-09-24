978ca9795a87e15f716624afe503233f6ba75174
carlos123488 <carlos.torrijos30@yahoo.com>
2026-09-23 20:42:52 -0400

    Added the intake subsystem. Configured the A button in gamepad 1 to intake.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotCentricTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RobotCentricTeleOp.java
@@ -34,7 +34,7 @@ public class RobotCentricTeleOp extends NextFTCOpMode {
     //private ShootingSystem shootingSystem;
     //private ShootingDirectionServo shootingDirectionServo;
     //private BallLoadingServo ballLoadingServo;
-    private Intake intakeSystem;
+    private Intake intakeSystem; //Sample Change
 
     // This is the "constructor" — runs once when the program starts loading
     public RobotCentricTeleOp() {
